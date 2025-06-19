9eedf3d4725972c787f9a2a3d601b11c0b478f97
ybpheno16 <yashubdl@gmail.com>
2025-06-19 23:58:00 +0530

    base fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:ProcessingInstruction.env.VITE_BASE_URL || "/intervue.io_frontend",
+  base: process.env.VITE_BASE_URL || "/intervue.io_frontend",
 })
