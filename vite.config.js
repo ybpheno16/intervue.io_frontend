1cc67977a6129a44b15c1c075beb6381c1d8e2a2
ybpheno16 <yashubdl@gmail.com>
2025-06-20 09:18:14 +0530

    update package.json to deploy and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || "/intervue.io_frontend",
+  base: "/intervue.io_frontend",
 })
